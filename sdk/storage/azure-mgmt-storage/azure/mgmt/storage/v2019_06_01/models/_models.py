395cdf4eda0d668abc1d627d2e5c9cf0d1223457
SDK Automation <sdkautomation@microsoft.com>
2020-03-19 21:06:29 +0000

    Generated from 8f47eb1e3909c551c8a97e279cbc66968119c5ea
    Support Infrastructure Encryption - secondary layer of encryption at REST for accounts.
    
    Add a boolean property to Microsoft.Storage/storageAccounts resource under encryption attributes to opt-in for a secondary layer of encryption for data at rest.

--- sdk/storage/azure-mgmt-storage/azure/mgmt/storage/v2019_06_01/models/_models.py
+++ sdk/storage/azure-mgmt-storage/azure/mgmt/storage/v2019_06_01/models/_models.py
@@ -835,6 +835,10 @@ class Encryption(Model):
      Possible values include: 'Microsoft.Storage', 'Microsoft.Keyvault'.
      Default value: "Microsoft.Storage" .
     :type key_source: str or ~azure.mgmt.storage.v2019_06_01.models.KeySource
+    :param require_infrastructure_encryption: A boolean indicating whether or
+     not the service applies a secondary layer of encryption with platform
+     managed keys for data at rest.
+    :type require_infrastructure_encryption: bool
     :param key_vault_properties: Properties provided by key vault.
     :type key_vault_properties:
      ~azure.mgmt.storage.v2019_06_01.models.KeyVaultProperties
@@ -847,6 +851,7 @@ class Encryption(Model):
     _attribute_map = {
         'services': {'key': 'services', 'type': 'EncryptionServices'},
         'key_source': {'key': 'keySource', 'type': 'str'},
+        'require_infrastructure_encryption': {'key': 'requireInfrastructureEncryption', 'type': 'bool'},
         'key_vault_properties': {'key': 'keyvaultproperties', 'type': 'KeyVaultProperties'},
     }
 
@@ -854,6 +859,7 @@ class Encryption(Model):
         super(Encryption, self).__init__(**kwargs)
         self.services = kwargs.get('services', None)
         self.key_source = kwargs.get('key_source', "Microsoft.Storage")
+        self.require_infrastructure_encryption = kwargs.get('require_infrastructure_encryption', None)
         self.key_vault_properties = kwargs.get('key_vault_properties', None)
 
 
@@ -1509,18 +1515,34 @@ class IPRule(Model):
 class KeyVaultProperties(Model):
     """Properties of key vault.
 
+    Variables are only populated by the server, and will be ignored when
+    sending a request.
+
     :param key_name: The name of KeyVault key.
     :type key_name: str
     :param key_version: The version of KeyVault key.
     :type key_version: str
     :param key_vault_uri: The Uri of KeyVault.
     :type key_vault_uri: str
+    :ivar current_versioned_key_identifier: The object identifier of the
+     current versioned Key Vault Key in use.
+    :vartype current_versioned_key_identifier: str
+    :ivar last_key_rotation_timestamp: Timestamp of last rotation of the Key
+     Vault Key.
+    :vartype last_key_rotation_timestamp: datetime
     """
+
+    _validation = {
+        'current_versioned_key_identifier': {'readonly': True},
+        'last_key_rotation_timestamp': {'readonly': True},
+    }
 
     _attribute_map = {
         'key_name': {'key': 'keyname', 'type': 'str'},
         'key_version': {'key': 'keyversion', 'type': 'str'},
         'key_vault_uri': {'key': 'keyvaulturi', 'type': 'str'},
+        'current_versioned_key_identifier': {'key': 'currentVersionedKeyIdentifier', 'type': 'str'},
+        'last_key_rotation_timestamp': {'key': 'lastKeyRotationTimestamp', 'type': 'iso-8601'},
     }
 
     def __init__(self, **kwargs):
@@ -1528,6 +1550,8 @@ class KeyVaultProperties(Model):
         self.key_name = kwargs.get('key_name', None)
         self.key_version = kwargs.get('key_version', None)
         self.key_vault_uri = kwargs.get('key_vault_uri', None)
+        self.current_versioned_key_identifier = None
+        self.last_key_rotation_timestamp = None
 
 
 class LeaseContainerRequest(Model):
