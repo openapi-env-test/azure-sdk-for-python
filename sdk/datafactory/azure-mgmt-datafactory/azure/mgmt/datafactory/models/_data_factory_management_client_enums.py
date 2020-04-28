9e934404cc852da6fd29220d9cd2623e40783609
SDK Automation <sdkautomation@microsoft.com>
2020-04-28 19:31:26 +0000

    Generated from ec763a9b55573bae406a37c753ac855fcd001cee
    Update readme files for python, go and java for API version 2020-03-01

--- sdk/datafactory/azure-mgmt-datafactory/azure/mgmt/datafactory/models/_data_factory_management_client_enums.py
+++ sdk/datafactory/azure-mgmt-datafactory/azure/mgmt/datafactory/models/_data_factory_management_client_enums.py
@@ -446,6 +446,13 @@ class TeradataPartitionOption(str, Enum):
     dynamic_range = "DynamicRange"
 
 
+class SqlPartitionOption(str, Enum):
+
+    none = "None"
+    physical_partitions_of_table = "PhysicalPartitionsOfTable"
+    dynamic_range = "DynamicRange"
+
+
 class StoredProcedureParameterType(str, Enum):
 
     string = "String"
