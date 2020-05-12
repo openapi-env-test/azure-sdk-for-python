625efa93d722a6b85d4959b4461352f873c20d93
SDK Automation <sdkautomation@microsoft.com>
2020-05-12 07:33:30 +0000

    Generated from b78703e13f3d23b69ff182e8efadbc4f45e0c023
    update cli.md

--- sdk/datafactory/azure-mgmt-datafactory/azure/mgmt/datafactory/aio/__init__.py
+++ sdk/datafactory/azure-mgmt-datafactory/azure/mgmt/datafactory/aio/__init__.py
@@ -6,14 +6,5 @@
 # Changes may cause incorrect behavior and will be lost if the code is regenerated.
 # --------------------------------------------------------------------------
 
-from ._data_factory_management_client import DataFactoryManagementClient
-from ._version import VERSION
-
-__version__ = VERSION
+from ._data_factory_management_client_async import DataFactoryManagementClient
 __all__ = ['DataFactoryManagementClient']
-
-try:
-    from ._patch import patch_sdk
-    patch_sdk()
-except ImportError:
-    pass
