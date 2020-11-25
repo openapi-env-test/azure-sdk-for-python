44206eaf14a931f59f04d4638e885008bb3204d4
Yuchao Yan <yuchaoyan@microsoft.com>
2020-11-25 17:06:23 +0800

    test

--- scripts/automation_init.py
+++ scripts/automation_init.py
@@ -7,21 +7,11 @@
 
 import sys
 from subprocess import call
-import os
-
-
-def myPrint(info):
-    print('XXXXXXXXXXXXXXX:' + info)
-
 
 if __name__ == '__main__':
     call('python scripts/dev_setup.py -p azure-core', shell=True)
-    for i in range(0, len(sys.argv)):
-        myPrint(sys.argv[i])
-    if len(sys.argv) == 4:
-        myPrint('sys.argv[3]:' + str(sys.argv[3]))
-        with open(sys.argv[3], 'w') as file_out:
+    if len(sys.argv) > 1:
+        with open(sys.argv[-1], 'w') as file_out:
             file_out.writelines(["{}\n"])
-            myPrint('output succeed!!!')
 
     print("Finished automation init.")
