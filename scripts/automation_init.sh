4735291f4d92199dc18849c4553bdd92a64ea38d
Yuchao Yan <yuchaoyan@microsoft.com>
2020-11-27 16:07:25 +0800

    shell

---
+++ scripts/automation_init.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+
+set - x
+set - e
+rm -rf ../venv-sdk
+python3 -m venv ../venv-sdk
+source ../venv-sdk/bin/activate
+python scripts/dev_setup.py -p azure-core
+python << EOF
+echo "{}" >> $2
