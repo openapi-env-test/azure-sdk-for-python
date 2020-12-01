a4d739a39385be919ac4ff4aa527c7d5d4f59d76
Yuchao Yan <yuchaoyan@microsoft.com>
2020-12-01 10:37:12 +0800

    shell

--- scripts/automation_generate.sh
+++ scripts/automation_generate.sh
@@ -4,5 +4,5 @@ VIRTUAL_ENV=$(pwd)/../venv-sdk
 export VIRTUAL_ENV
 PATH="$VIRTUAL_ENV/bin:$PATH"
 export PATH
-python -m packaging_tools.generate_script_v2 $1 $2
+python -m packaging_tools.generate_script_v2 "$1" "$2"
 echo "[Generate] generate success!!!"
