4e0c7edffa600047eaa3e5c708c1d94feecf64e1
Yuchao Yan <yuchaoyan@microsoft.com>
2020-12-29 18:13:57 +0800

    debug

--- scripts/automation_generate.sh
+++ scripts/automation_generate.sh
@@ -4,6 +4,11 @@ VIRTUAL_ENV=$(pwd)/../venv-sdk
 export VIRTUAL_ENV
 PATH="$VIRTUAL_ENV/bin:$PATH"
 export PATH
+
+sudo apt-get remove nodejs
+curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash  -
+sudo apt-get install -y nodejs
+
 python -m packaging_tools.auto_codegen "$1" "../venv-sdk/auto_temp.json" 2>&1
 echo "[Generate] codegen done!!!"
 python -m packaging_tools.auto_package "../venv-sdk/auto_temp.json" "$2" 2>&1
