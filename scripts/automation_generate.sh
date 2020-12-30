147304e2470583a3aa739c332e414f2e5b61dee7
Yuchao Yan <yuchaoyan@microsoft.com>
2020-12-30 17:37:04 +0800

    debug

--- scripts/automation_generate.sh
+++ scripts/automation_generate.sh
@@ -5,6 +5,10 @@ export VIRTUAL_ENV
 PATH="$VIRTUAL_ENV/bin:$PATH"
 export PATH
 
+# node version degrade
+sudo npm install -g n
+sudo n 10.15.0
+export PATH=/usr/local/n/versions/node/10.15.0/bin:$PATH
 
 # generate code and package
 python -m packaging_tools.auto_codegen "$1" "../venv-sdk/auto_temp.json" 2>&1
--- scripts/automation_init.sh
+++ scripts/automation_init.sh
@@ -8,11 +8,4 @@ PATH="$VIRTUAL_ENV/bin:$PATH"
 export PATH
 python scripts/dev_setup.py -p azure-core
 echo "{}" >> $2
-
-# node version degrade
-sudo npm install -g n
-sudo n 10.15.0
-export PATH=/usr/local/n/versions/node/10.15.0/bin:$PATH
-node -v
-
 echo "[Generate] init success!!!"
