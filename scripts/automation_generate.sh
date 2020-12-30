9842099f1abb8fd806b8f50df9310e195a0c8007
Yuchao Yan <yuchaoyan@microsoft.com>
2020-12-30 17:09:56 +0800

    debug

--- scripts/automation_generate.sh
+++ scripts/automation_generate.sh
@@ -5,9 +5,6 @@ export VIRTUAL_ENV
 PATH="$VIRTUAL_ENV/bin:$PATH"
 export PATH
 
-# node version degrade
-export PATH=/usr/local/n/versions/node/10.15.0/bin:$PATH
-node -v
 
 # generate code and package
 python -m packaging_tools.auto_codegen "$1" "../venv-sdk/auto_temp.json" 2>&1
