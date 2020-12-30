bde354e8356c93c4a35394767c3cbf007c2885d5
Yuchao Yan <yuchaoyan@microsoft.com>
2020-12-30 16:42:34 +0800

    debug

--- scripts/automation_init.sh
+++ scripts/automation_init.sh
@@ -8,4 +8,10 @@ PATH="$VIRTUAL_ENV/bin:$PATH"
 export PATH
 python scripts/dev_setup.py -p azure-core
 echo "{}" >> $2
+
+# node version degrade
+sudo npm install -g n
+sudo n 10.15.0
+node -v
+
 echo "[Generate] init success!!!"
