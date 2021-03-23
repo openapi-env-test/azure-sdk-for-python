ada7d2af5892a151145f8b05b61f6b952c91bc31
SDKAuto <sdkautomation@microsoft.com>
2021-03-23 01:14:23 +0000

    CodeGen from PR 1705 in openapi-env-test/azure-rest-api-specs Merge e6303daec9f3d47baa1bd05042b099b0c9f4c37e into d32923f522478e80a7ed196ceb1eaf3ceecfeb4d

--- sdk/attestation/azure-mgmt-attestation/azure/mgmt/attestation/models/_models_py3.py
+++ sdk/attestation/azure-mgmt-attestation/azure/mgmt/attestation/models/_models_py3.py
@@ -651,8 +651,7 @@ class SystemData(Model):
      'ManagedIdentity', 'Key'
     :type last_modified_by_type: str or
      ~azure.mgmt.attestation.models.CreatedByType
-    :param last_modified_at: The type of identity that last modified the
-     resource.
+    :param last_modified_at: The timestamp of resource last modification (UTC)
     :type last_modified_at: datetime
     """
 
