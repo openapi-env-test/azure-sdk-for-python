8d07d3643bd7fd7c5d0aad086fe270bebc8dcb7d
SDKAuto <sdkautomation@microsoft.com>
2021-05-13 05:02:14 +0000

    CodeGen from PR 2881 in openapi-env-test/azure-rest-api-specs Merge 9ce9b5860d4bb466f45722646b26591c75ceff6e into a45f1f0e23c797cc9a58b70a2238a1acf95310f4

--- sdk/appplatform/azure-mgmt-appplatform/azure/mgmt/appplatform/v2020_07_01/operations/_services_operations.py
+++ sdk/appplatform/azure-mgmt-appplatform/azure/mgmt/appplatform/v2020_07_01/operations/_services_operations.py
@@ -54,7 +54,7 @@ class ServicesOperations(object):
         **kwargs  # type: Any
     ):
         # type: (...) -> "_models.ServiceResource"
-        """Get a Service and its properties.
+        """Get a Service and its properties. Test. Test2 d.
 
         :param resource_group_name: The name of the resource group that contains the resource. You can
          obtain this value from the Azure Resource Manager API or the portal.
