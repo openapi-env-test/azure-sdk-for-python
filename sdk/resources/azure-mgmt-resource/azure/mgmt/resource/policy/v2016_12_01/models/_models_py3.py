448b1e6b3c7da4560258e307c65fa8203f323060
SDKAuto <sdkautomation@microsoft.com>
2021-05-18 02:42:07 +0000

    CodeGen from PR 2891 in openapi-env-test/azure-rest-api-specs Merge 48e16fb33fe92d2cc8bd49691ad313f14300a9a5 into 7e1052f7e825050000230cdd4841c4748b4d3370

--- sdk/resources/azure-mgmt-resource/azure/mgmt/resource/policy/v2016_12_01/models/_models_py3.py
+++ sdk/resources/azure-mgmt-resource/azure/mgmt/resource/policy/v2016_12_01/models/_models_py3.py
@@ -31,7 +31,7 @@ class PolicyAssignment(msrest.serialization.Model):
     :param scope: The scope for the policy assignment.
     :type scope: str
     :param parameters: Required if a parameter is used in policy rule.
-    :type parameters: str
+    :type parameters: object
     :param description: This message will be part of response in case of policy violation.
     :type description: str
     """
@@ -47,7 +47,7 @@ class PolicyAssignment(msrest.serialization.Model):
         'display_name': {'key': 'properties.displayName', 'type': 'str'},
         'policy_definition_id': {'key': 'properties.policyDefinitionId', 'type': 'str'},
         'scope': {'key': 'properties.scope', 'type': 'str'},
-        'parameters': {'key': 'properties.parameters', 'type': 'str'},
+        'parameters': {'key': 'properties.parameters', 'type': 'object'},
         'description': {'key': 'properties.description', 'type': 'str'},
     }
 
@@ -59,7 +59,7 @@ class PolicyAssignment(msrest.serialization.Model):
         display_name: Optional[str] = None,
         policy_definition_id: Optional[str] = None,
         scope: Optional[str] = None,
-        parameters: Optional[str] = None,
+        parameters: Optional[object] = None,
         description: Optional[str] = None,
         **kwargs
     ):
@@ -120,11 +120,11 @@ class PolicyDefinition(msrest.serialization.Model):
     :param description: The policy definition description.
     :type description: str
     :param policy_rule: The policy rule.
-    :type policy_rule: str
+    :type policy_rule: object
     :param metadata: The policy definition metadata.
-    :type metadata: str
+    :type metadata: object
     :param parameters: Required if a parameter is used in policy rule.
-    :type parameters: str
+    :type parameters: object
     """
 
     _validation = {
@@ -139,9 +139,9 @@ class PolicyDefinition(msrest.serialization.Model):
         'mode': {'key': 'properties.mode', 'type': 'str'},
         'display_name': {'key': 'properties.displayName', 'type': 'str'},
         'description': {'key': 'properties.description', 'type': 'str'},
-        'policy_rule': {'key': 'properties.policyRule', 'type': 'str'},
-        'metadata': {'key': 'properties.metadata', 'type': 'str'},
-        'parameters': {'key': 'properties.parameters', 'type': 'str'},
+        'policy_rule': {'key': 'properties.policyRule', 'type': 'object'},
+        'metadata': {'key': 'properties.metadata', 'type': 'object'},
+        'parameters': {'key': 'properties.parameters', 'type': 'object'},
     }
 
     def __init__(
@@ -151,9 +151,9 @@ class PolicyDefinition(msrest.serialization.Model):
         mode: Optional[Union[str, "PolicyMode"]] = None,
         display_name: Optional[str] = None,
         description: Optional[str] = None,
-        policy_rule: Optional[str] = None,
-        metadata: Optional[str] = None,
-        parameters: Optional[str] = None,
+        policy_rule: Optional[object] = None,
+        metadata: Optional[object] = None,
+        parameters: Optional[object] = None,
         **kwargs
     ):
         super(PolicyDefinition, self).__init__(**kwargs)
