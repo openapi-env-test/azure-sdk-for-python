27dd7e1317b70221ff0280dd414dd66f8528fe1c
SDKAuto <sdkautomation@microsoft.com>
2021-08-15 10:42:55 +0000

    CodeGen from PR 2966 in openapi-env-test/azure-rest-api-specs Merge 5022628235c421541fd2993ad037d7f68f0c00e5 into ca836010ff623e8e304a11411ea321772163f042

--- sdk/agrifood/azure-mgmt-agrifood/azure/mgmt/agrifood/_azure_agri_food_rp_service.py
+++ sdk/agrifood/azure-mgmt-agrifood/azure/mgmt/agrifood/_azure_agri_food_rp_service.py
@@ -28,7 +28,7 @@ from . import models
 
 
 class AzureAgriFoodRPService(object):
-    """APIs documentation for Azure AgriFood Resource Provider Service.
+    """APIs documentation for Azure AgriFood Resource Provider Service. Test.
 
     :ivar extensions: ExtensionsOperations operations
     :vartype extensions: azure.mgmt.agrifood.operations.ExtensionsOperations
