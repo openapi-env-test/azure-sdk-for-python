f40b88711e0e6eddae6e319333fe665dfe26b6b9
SDKAuto <sdkautomation@microsoft.com>
2022-07-07 05:24:43 +0000

    CodeGen from PR 3160 in openapi-env-test/azure-rest-api-specs Merge 0dfeb83ddd0eb942f20a1d4b8fddb75b0111ac17 into 9facb1d3a8b1053b984f2776ea37d529c202812e

--- sdk/chaos/azure-mgmt-chaos/azure/mgmt/chaos/_chaos_management_client.py
+++ sdk/chaos/azure-mgmt-chaos/azure/mgmt/chaos/_chaos_management_client.py
@@ -9,20 +9,27 @@
 from copy import deepcopy
 from typing import Any, TYPE_CHECKING
 
-from msrest import Deserializer, Serializer
-
 from azure.core.rest import HttpRequest, HttpResponse
 from azure.mgmt.core import ARMPipelineClient
 
 from . import models
 from ._configuration import ChaosManagementClientConfiguration
-from .operations import CapabilitiesOperations, CapabilityTypesOperations, ExperimentsOperations, Operations, TargetTypesOperations, TargetsOperations
+from ._serialization import Deserializer, Serializer
+from .operations import (
+    CapabilitiesOperations,
+    CapabilityTypesOperations,
+    ExperimentsOperations,
+    Operations,
+    TargetTypesOperations,
+    TargetsOperations,
+)
 
 if TYPE_CHECKING:
     # pylint: disable=unused-import,ungrouped-imports
     from azure.core.credentials import TokenCredential
 
-class ChaosManagementClient:
+
+class ChaosManagementClient:  # pylint: disable=client-accepts-api-version-keyword
     """Chaos Management Client.
 
     :ivar capabilities: CapabilitiesOperations operations
@@ -37,9 +44,9 @@ class ChaosManagementClient:
     :vartype target_types: azure.mgmt.chaos.operations.TargetTypesOperations
     :ivar capability_types: CapabilityTypesOperations operations
     :vartype capability_types: azure.mgmt.chaos.operations.CapabilityTypesOperations
-    :param credential: Credential needed for the client to connect to Azure.
+    :param credential: Credential needed for the client to connect to Azure. Required.
     :type credential: ~azure.core.credentials.TokenCredential
-    :param subscription_id: GUID that represents an Azure subscription ID.
+    :param subscription_id: GUID that represents an Azure subscription ID. Required.
     :type subscription_id: str
     :param base_url: Service URL. Default value is "https://management.azure.com".
     :type base_url: str
@@ -57,38 +64,25 @@ class ChaosManagementClient:
         base_url: str = "https://management.azure.com",
         **kwargs: Any
     ) -> None:
-        self._config = ChaosManagementClientConfiguration(credential=credential, subscription_id=subscription_id, **kwargs)
+        self._config = ChaosManagementClientConfiguration(
+            credential=credential, subscription_id=subscription_id, **kwargs
+        )
         self._client = ARMPipelineClient(base_url=base_url, config=self._config, **kwargs)
 
         client_models = {k: v for k, v in models.__dict__.items() if isinstance(v, type)}
         self._serialize = Serializer(client_models)
         self._deserialize = Deserializer(client_models)
         self._serialize.client_side_validation = False
-        self.capabilities = CapabilitiesOperations(
-            self._client, self._config, self._serialize, self._deserialize
-        )
-        self.experiments = ExperimentsOperations(
-            self._client, self._config, self._serialize, self._deserialize
-        )
-        self.operations = Operations(
-            self._client, self._config, self._serialize, self._deserialize
-        )
-        self.targets = TargetsOperations(
-            self._client, self._config, self._serialize, self._deserialize
-        )
-        self.target_types = TargetTypesOperations(
-            self._client, self._config, self._serialize, self._deserialize
-        )
+        self.capabilities = CapabilitiesOperations(self._client, self._config, self._serialize, self._deserialize)
+        self.experiments = ExperimentsOperations(self._client, self._config, self._serialize, self._deserialize)
+        self.operations = Operations(self._client, self._config, self._serialize, self._deserialize)
+        self.targets = TargetsOperations(self._client, self._config, self._serialize, self._deserialize)
+        self.target_types = TargetTypesOperations(self._client, self._config, self._serialize, self._deserialize)
         self.capability_types = CapabilityTypesOperations(
             self._client, self._config, self._serialize, self._deserialize
         )
 
-
-    def _send_request(
-        self,
-        request: HttpRequest,
-        **kwargs: Any
-    ) -> HttpResponse:
+    def _send_request(self, request: HttpRequest, **kwargs: Any) -> HttpResponse:
         """Runs the network request through the client's chained policies.
 
         >>> from azure.core.rest import HttpRequest
@@ -97,7 +91,7 @@ class ChaosManagementClient:
         >>> response = client._send_request(request)
         <HttpResponse: 200 OK>
 
-        For more information on this code flow, see https://aka.ms/azsdk/python/protocol/quickstart
+        For more information on this code flow, see https://aka.ms/azsdk/dpcodegen/python/send_request
 
         :param request: The network request you want to make. Required.
         :type request: ~azure.core.rest.HttpRequest
--- sdk/chaos/azure-mgmt-chaos/azure/mgmt/chaos/operations/_capability_types_operations.py
+++ sdk/chaos/azure-mgmt-chaos/azure/mgmt/chaos/operations/_capability_types_operations.py
@@ -7,10 +7,15 @@
 # Changes may cause incorrect behavior and will be lost if the code is regenerated.
 # --------------------------------------------------------------------------
 from typing import Any, Callable, Dict, Iterable, Optional, TypeVar
+from urllib.parse import parse_qs, urljoin, urlparse
 
-from msrest import Serializer
-
-from azure.core.exceptions import ClientAuthenticationError, HttpResponseError, ResourceExistsError, ResourceNotFoundError, map_error
+from azure.core.exceptions import (
+    ClientAuthenticationError,
+    HttpResponseError,
+    ResourceExistsError,
+    ResourceNotFoundError,
+    map_error,
+)
 from azure.core.paging import ItemPaged
 from azure.core.pipeline import PipelineResponse
 from azure.core.pipeline.transport import HttpResponse
@@ -20,17 +25,20 @@ from azure.core.utils import case_insensitive_dict
 from azure.mgmt.core.exceptions import ARMErrorFormat
 
 from .. import models as _models
+from .._serialization import Serializer
 from .._vendor import _convert_request, _format_url_section
-T = TypeVar('T')
+
+T = TypeVar("T")
 ClsType = Optional[Callable[[PipelineResponse[HttpRequest, HttpResponse], T, Dict[str, Any]], Any]]
 
 _SERIALIZER = Serializer()
 _SERIALIZER.client_side_validation = False
 
+
 def build_list_request(
-    subscription_id: str,
     location_name: str,
     target_type_name: str,
+    subscription_id: str,
     *,
     continuation_token_parameter: Optional[str] = None,
     **kwargs: Any
@@ -38,73 +46,78 @@ def build_list_request(
     _headers = case_insensitive_dict(kwargs.pop("headers", {}) or {})
     _params = case_insensitive_dict(kwargs.pop("params", {}) or {})
 
-    api_version = kwargs.pop('api_version', _params.pop('api-version', "2021-09-15-preview"))  # type: str
-    accept = _headers.pop('Accept', "application/json")
+    api_version = kwargs.pop("api_version", _params.pop("api-version", "2021-09-15-preview"))  # type: str
+    accept = _headers.pop("Accept", "application/json")
 
     # Construct URL
-    _url = kwargs.pop("template_url", "/subscriptions/{subscriptionId}/providers/Microsoft.Chaos/locations/{locationName}/targetTypes/{targetTypeName}/capabilityTypes")  # pylint: disable=line-too-long
+    _url = kwargs.pop(
+        "template_url",
+        "/subscriptions/{subscriptionId}/providers/Microsoft.Chaos/locations/{locationName}/targetTypes/{targetTypeName}/capabilityTypes",
+    )  # pylint: disable=line-too-long
     path_format_arguments = {
-        "subscriptionId": _SERIALIZER.url("subscription_id", subscription_id, 'str', pattern=r'^[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}$'),
-        "locationName": _SERIALIZER.url("location_name", location_name, 'str', pattern=r'^[a-zA-Z0-9_\-\.]+$'),
-        "targetTypeName": _SERIALIZER.url("target_type_name", target_type_name, 'str', pattern=r'^[a-zA-Z0-9_\-\.]+$'),
+        "subscriptionId": _SERIALIZER.url(
+            "subscription_id",
+            subscription_id,
+            "str",
+            pattern=r"^[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}$",
+        ),
+        "locationName": _SERIALIZER.url("location_name", location_name, "str", pattern=r"^[a-zA-Z0-9_\-\.]+$"),
+        "targetTypeName": _SERIALIZER.url("target_type_name", target_type_name, "str", pattern=r"^[a-zA-Z0-9_\-\.]+$"),
     }
 
     _url = _format_url_section(_url, **path_format_arguments)
 
     # Construct parameters
-    _params['api-version'] = _SERIALIZER.query("api_version", api_version, 'str')
+    _params["api-version"] = _SERIALIZER.query("api_version", api_version, "str")
     if continuation_token_parameter is not None:
-        _params['continuationToken'] = _SERIALIZER.query("continuation_token_parameter", continuation_token_parameter, 'str')
+        _params["continuationToken"] = _SERIALIZER.query(
+            "continuation_token_parameter", continuation_token_parameter, "str"
+        )
 
     # Construct headers
-    _headers['Accept'] = _SERIALIZER.header("accept", accept, 'str')
+    _headers["Accept"] = _SERIALIZER.header("accept", accept, "str")
 
-    return HttpRequest(
-        method="GET",
-        url=_url,
-        params=_params,
-        headers=_headers,
-        **kwargs
-    )
+    return HttpRequest(method="GET", url=_url, params=_params, headers=_headers, **kwargs)
 
 
 def build_get_request(
-    subscription_id: str,
-    location_name: str,
-    target_type_name: str,
-    capability_type_name: str,
-    **kwargs: Any
+    location_name: str, target_type_name: str, capability_type_name: str, subscription_id: str, **kwargs: Any
 ) -> HttpRequest:
     _headers = case_insensitive_dict(kwargs.pop("headers", {}) or {})
     _params = case_insensitive_dict(kwargs.pop("params", {}) or {})
 
-    api_version = kwargs.pop('api_version', _params.pop('api-version', "2021-09-15-preview"))  # type: str
-    accept = _headers.pop('Accept', "application/json")
+    api_version = kwargs.pop("api_version", _params.pop("api-version", "2021-09-15-preview"))  # type: str
+    accept = _headers.pop("Accept", "application/json")
 
     # Construct URL
-    _url = kwargs.pop("template_url", "/subscriptions/{subscriptionId}/providers/Microsoft.Chaos/locations/{locationName}/targetTypes/{targetTypeName}/capabilityTypes/{capabilityTypeName}")  # pylint: disable=line-too-long
+    _url = kwargs.pop(
+        "template_url",
+        "/subscriptions/{subscriptionId}/providers/Microsoft.Chaos/locations/{locationName}/targetTypes/{targetTypeName}/capabilityTypes/{capabilityTypeName}",
+    )  # pylint: disable=line-too-long
     path_format_arguments = {
-        "subscriptionId": _SERIALIZER.url("subscription_id", subscription_id, 'str', pattern=r'^[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}$'),
-        "locationName": _SERIALIZER.url("location_name", location_name, 'str', pattern=r'^[a-zA-Z0-9_\-\.]+$'),
-        "targetTypeName": _SERIALIZER.url("target_type_name", target_type_name, 'str', pattern=r'^[a-zA-Z0-9_\-\.]+$'),
-        "capabilityTypeName": _SERIALIZER.url("capability_type_name", capability_type_name, 'str', pattern=r'^[a-zA-Z0-9\-\.]+-\d\.\d$'),
+        "subscriptionId": _SERIALIZER.url(
+            "subscription_id",
+            subscription_id,
+            "str",
+            pattern=r"^[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}$",
+        ),
+        "locationName": _SERIALIZER.url("location_name", location_name, "str", pattern=r"^[a-zA-Z0-9_\-\.]+$"),
+        "targetTypeName": _SERIALIZER.url("target_type_name", target_type_name, "str", pattern=r"^[a-zA-Z0-9_\-\.]+$"),
+        "capabilityTypeName": _SERIALIZER.url(
+            "capability_type_name", capability_type_name, "str", pattern=r"^[a-zA-Z0-9\-\.]+-\d\.\d$"
+        ),
     }
 
     _url = _format_url_section(_url, **path_format_arguments)
 
     # Construct parameters
-    _params['api-version'] = _SERIALIZER.query("api_version", api_version, 'str')
+    _params["api-version"] = _SERIALIZER.query("api_version", api_version, "str")
 
     # Construct headers
-    _headers['Accept'] = _SERIALIZER.header("accept", accept, 'str')
+    _headers["Accept"] = _SERIALIZER.header("accept", accept, "str")
 
-    return HttpRequest(
-        method="GET",
-        url=_url,
-        params=_params,
-        headers=_headers,
-        **kwargs
-    )
+    return HttpRequest(method="GET", url=_url, params=_params, headers=_headers, **kwargs)
+
 
 class CapabilityTypesOperations:
     """
@@ -125,7 +138,6 @@ class CapabilityTypesOperations:
         self._serialize = input_args.pop(0) if input_args else kwargs.pop("serializer")
         self._deserialize = input_args.pop(0) if input_args else kwargs.pop("deserializer")
 
-
     @distributed_trace
     def list(
         self,
@@ -133,42 +145,40 @@ class CapabilityTypesOperations:
         target_type_name: str,
         continuation_token_parameter: Optional[str] = None,
         **kwargs: Any
-    ) -> Iterable[_models.CapabilityTypeListResult]:
+    ) -> Iterable["_models.CapabilityType"]:
         """Get a list of Capability Type resources for given Target Type and location.
 
-        :param location_name: String that represents a Location resource name.
+        :param location_name: String that represents a Location resource name. Required.
         :type location_name: str
-        :param target_type_name: String that represents a Target Type resource name.
+        :param target_type_name: String that represents a Target Type resource name. Required.
         :type target_type_name: str
         :param continuation_token_parameter: String that sets the continuation token. Default value is
          None.
         :type continuation_token_parameter: str
         :keyword callable cls: A custom type or function that will be passed the direct response
-        :return: An iterator like instance of either CapabilityTypeListResult or the result of
-         cls(response)
-        :rtype: ~azure.core.paging.ItemPaged[~azure.mgmt.chaos.models.CapabilityTypeListResult]
-        :raises: ~azure.core.exceptions.HttpResponseError
+        :return: An iterator like instance of either CapabilityType or the result of cls(response)
+        :rtype: ~azure.core.paging.ItemPaged[~azure.mgmt.chaos.models.CapabilityType]
+        :raises ~azure.core.exceptions.HttpResponseError:
         """
         _headers = kwargs.pop("headers", {}) or {}
         _params = case_insensitive_dict(kwargs.pop("params", {}) or {})
 
-        api_version = kwargs.pop('api_version', _params.pop('api-version', "2021-09-15-preview"))  # type: str
-        cls = kwargs.pop('cls', None)  # type: ClsType[_models.CapabilityTypeListResult]
+        api_version = kwargs.pop("api_version", _params.pop("api-version", self._config.api_version))  # type: str
+        cls = kwargs.pop("cls", None)  # type: ClsType[_models.CapabilityTypeListResult]
 
-        error_map = {
-            401: ClientAuthenticationError, 404: ResourceNotFoundError, 409: ResourceExistsError
-        }
-        error_map.update(kwargs.pop('error_map', {}) or {})
+        error_map = {401: ClientAuthenticationError, 404: ResourceNotFoundError, 409: ResourceExistsError}
+        error_map.update(kwargs.pop("error_map", {}) or {})
+
         def prepare_request(next_link=None):
             if not next_link:
-                
+
                 request = build_list_request(
-                    subscription_id=self._config.subscription_id,
                     location_name=location_name,
                     target_type_name=target_type_name,
-                    api_version=api_version,
+                    subscription_id=self._config.subscription_id,
                     continuation_token_parameter=continuation_token_parameter,
-                    template_url=self.list.metadata['url'],
+                    api_version=api_version,
+                    template_url=self.list.metadata["url"],
                     headers=_headers,
                     params=_params,
                 )
@@ -176,17 +186,11 @@ class CapabilityTypesOperations:
                 request.url = self._client.format_url(request.url)  # type: ignore
 
             else:
-                
-                request = build_list_request(
-                    subscription_id=self._config.subscription_id,
-                    location_name=location_name,
-                    target_type_name=target_type_name,
-                    api_version=api_version,
-                    continuation_token_parameter=continuation_token_parameter,
-                    template_url=next_link,
-                    headers=_headers,
-                    params=_params,
-                )
+                # make call to next link with the client's api-version
+                _parsed_next_link = urlparse(next_link)
+                _next_request_params = case_insensitive_dict(parse_qs(_parsed_next_link.query))
+                _next_request_params["api-version"] = self._config.api_version
+                request = HttpRequest("GET", urljoin(next_link, _parsed_next_link.path), params=_next_request_params)
                 request = _convert_request(request)
                 request.url = self._client.format_url(request.url)  # type: ignore
                 request.method = "GET"
@@ -202,10 +206,8 @@ class CapabilityTypesOperations:
         def get_next(next_link=None):
             request = prepare_request(next_link)
 
-            pipeline_response = self._client._pipeline.run(  # pylint: disable=protected-access
-                request,
-                stream=False,
-                **kwargs
+            pipeline_response = self._client._pipeline.run(  # type: ignore # pylint: disable=protected-access
+                request, stream=False, **kwargs
             )
             response = pipeline_response.http_response
 
@@ -216,52 +218,43 @@ class CapabilityTypesOperations:
 
             return pipeline_response
 
+        return ItemPaged(get_next, extract_data)
 
-        return ItemPaged(
-            get_next, extract_data
-        )
-    list.metadata = {'url': "/subscriptions/{subscriptionId}/providers/Microsoft.Chaos/locations/{locationName}/targetTypes/{targetTypeName}/capabilityTypes"}  # type: ignore
+    list.metadata = {"url": "/subscriptions/{subscriptionId}/providers/Microsoft.Chaos/locations/{locationName}/targetTypes/{targetTypeName}/capabilityTypes"}  # type: ignore
 
     @distributed_trace
     def get(
-        self,
-        location_name: str,
-        target_type_name: str,
-        capability_type_name: str,
-        **kwargs: Any
+        self, location_name: str, target_type_name: str, capability_type_name: str, **kwargs: Any
     ) -> _models.CapabilityType:
         """Get a Capability Type resource for given Target Type and location.
 
-        :param location_name: String that represents a Location resource name.
+        :param location_name: String that represents a Location resource name. Required.
         :type location_name: str
-        :param target_type_name: String that represents a Target Type resource name.
+        :param target_type_name: String that represents a Target Type resource name. Required.
         :type target_type_name: str
-        :param capability_type_name: String that represents a Capability Type resource name.
+        :param capability_type_name: String that represents a Capability Type resource name. Required.
         :type capability_type_name: str
         :keyword callable cls: A custom type or function that will be passed the direct response
-        :return: CapabilityType, or the result of cls(response)
+        :return: CapabilityType or the result of cls(response)
         :rtype: ~azure.mgmt.chaos.models.CapabilityType
-        :raises: ~azure.core.exceptions.HttpResponseError
+        :raises ~azure.core.exceptions.HttpResponseError:
         """
-        error_map = {
-            401: ClientAuthenticationError, 404: ResourceNotFoundError, 409: ResourceExistsError
-        }
-        error_map.update(kwargs.pop('error_map', {}) or {})
+        error_map = {401: ClientAuthenticationError, 404: ResourceNotFoundError, 409: ResourceExistsError}
+        error_map.update(kwargs.pop("error_map", {}) or {})
 
         _headers = kwargs.pop("headers", {}) or {}
         _params = case_insensitive_dict(kwargs.pop("params", {}) or {})
 
-        api_version = kwargs.pop('api_version', _params.pop('api-version', "2021-09-15-preview"))  # type: str
-        cls = kwargs.pop('cls', None)  # type: ClsType[_models.CapabilityType]
+        api_version = kwargs.pop("api_version", _params.pop("api-version", self._config.api_version))  # type: str
+        cls = kwargs.pop("cls", None)  # type: ClsType[_models.CapabilityType]
 
-        
         request = build_get_request(
-            subscription_id=self._config.subscription_id,
             location_name=location_name,
             target_type_name=target_type_name,
             capability_type_name=capability_type_name,
+            subscription_id=self._config.subscription_id,
             api_version=api_version,
-            template_url=self.get.metadata['url'],
+            template_url=self.get.metadata["url"],
             headers=_headers,
             params=_params,
         )
@@ -269,10 +262,9 @@ class CapabilityTypesOperations:
         request.url = self._client.format_url(request.url)  # type: ignore
 
         pipeline_response = self._client._pipeline.run(  # type: ignore # pylint: disable=protected-access
-            request,
-            stream=False,
-            **kwargs
+            request, stream=False, **kwargs
         )
+
         response = pipeline_response.http_response
 
         if response.status_code not in [200]:
@@ -280,12 +272,11 @@ class CapabilityTypesOperations:
             error = self._deserialize.failsafe_deserialize(_models.ErrorResponse, pipeline_response)
             raise HttpResponseError(response=response, model=error, error_format=ARMErrorFormat)
 
-        deserialized = self._deserialize('CapabilityType', pipeline_response)
+        deserialized = self._deserialize("CapabilityType", pipeline_response)
 
         if cls:
             return cls(pipeline_response, deserialized, {})
 
         return deserialized
 
-    get.metadata = {'url': "/subscriptions/{subscriptionId}/providers/Microsoft.Chaos/locations/{locationName}/targetTypes/{targetTypeName}/capabilityTypes/{capabilityTypeName}"}  # type: ignore
-
+    get.metadata = {"url": "/subscriptions/{subscriptionId}/providers/Microsoft.Chaos/locations/{locationName}/targetTypes/{targetTypeName}/capabilityTypes/{capabilityTypeName}"}  # type: ignore
