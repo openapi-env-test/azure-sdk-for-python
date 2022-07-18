a277e423ce24e5b0cab1c5b064000605515e2313
SDKAuto <sdkautomation@microsoft.com>
2022-07-18 14:51:41 +0000

    CodeGen from PR 3234 in openapi-env-test/azure-rest-api-specs Merge 59771d3124ba508e680667ac179274ebe89adcd0 into 22f3d52874ebf9d4343d70eea36da39a2504f335

--- sdk/keyvault/azure-mgmt-keyvault/azure/mgmt/keyvault/v2021_04_01_preview/aio/operations/__init__.py
+++ sdk/keyvault/azure-mgmt-keyvault/azure/mgmt/keyvault/v2021_04_01_preview/aio/operations/__init__.py
@@ -14,12 +14,18 @@ from ._mhsm_private_endpoint_connections_operations import MHSMPrivateEndpointCo
 from ._mhsm_private_link_resources_operations import MHSMPrivateLinkResourcesOperations
 from ._operations import Operations
 
+from ._patch import __all__ as _patch_all
+from ._patch import *  # type: ignore # pylint: disable=unused-wildcard-import
+from ._patch import patch_sdk as _patch_sdk
+
 __all__ = [
-    'VaultsOperations',
-    'PrivateEndpointConnectionsOperations',
-    'PrivateLinkResourcesOperations',
-    'ManagedHsmsOperations',
-    'MHSMPrivateEndpointConnectionsOperations',
-    'MHSMPrivateLinkResourcesOperations',
-    'Operations',
+    "VaultsOperations",
+    "PrivateEndpointConnectionsOperations",
+    "PrivateLinkResourcesOperations",
+    "ManagedHsmsOperations",
+    "MHSMPrivateEndpointConnectionsOperations",
+    "MHSMPrivateLinkResourcesOperations",
+    "Operations",
 ]
+__all__.extend([p for p in _patch_all if p not in __all__])
+_patch_sdk()
