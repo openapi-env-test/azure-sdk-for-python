ef76f7825844db10fd10eb27cb2a367b6aa0fdaa
SDKAuto <sdkautomation@microsoft.com>
2022-07-19 14:37:47 +0000

    CodeGen from PR 3305 in openapi-env-test/azure-rest-api-specs Merge cb1593364fc8f51f8b7f039bb1fa219a1c09a973 into e574317dfe6578abe1a588970ed6273d61b98d3c

--- sdk/chaos/azure-mgmt-chaos/azure/mgmt/chaos/aio/operations/__init__.py
+++ sdk/chaos/azure-mgmt-chaos/azure/mgmt/chaos/aio/operations/__init__.py
@@ -16,13 +16,14 @@ from ._capability_types_operations import CapabilityTypesOperations
 from ._patch import __all__ as _patch_all
 from ._patch import *  # type: ignore # pylint: disable=unused-wildcard-import
 from ._patch import patch_sdk as _patch_sdk
+
 __all__ = [
-    'CapabilitiesOperations',
-    'ExperimentsOperations',
-    'Operations',
-    'TargetsOperations',
-    'TargetTypesOperations',
-    'CapabilityTypesOperations',
+    "CapabilitiesOperations",
+    "ExperimentsOperations",
+    "Operations",
+    "TargetsOperations",
+    "TargetTypesOperations",
+    "CapabilityTypesOperations",
 ]
 __all__.extend([p for p in _patch_all if p not in __all__])
 _patch_sdk()
