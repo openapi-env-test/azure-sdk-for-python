377696fce7f1e153453d0029d76dfe94f314d279
azure-sdk <no-reply@azure-sdk.com>
2022-07-22 11:00:40 +0000

    [auto]create by sdk generation

--- sdk/deviceupdate/azure-iot-deviceupdate/azure/iot/deviceupdate/aio/_client.py
+++ sdk/deviceupdate/azure-iot-deviceupdate/azure/iot/deviceupdate/aio/_client.py
@@ -7,14 +7,15 @@
 # --------------------------------------------------------------------------
 
 from copy import deepcopy
-from typing import Any, Awaitable, Optional, TYPE_CHECKING
+from typing import Any, Awaitable, TYPE_CHECKING
+
+from msrest import Deserializer, Serializer
 
 from azure.core import AsyncPipelineClient
 from azure.core.rest import AsyncHttpResponse, HttpRequest
-from msrest import Deserializer, Serializer
 
 from ._configuration import DeviceUpdateClientConfiguration
-from .operations import DeviceManagementOperations, DeviceUpdateOperations
+from .operations import DeploymentsOperations, DevicesOperations, UpdatesOperations
 
 if TYPE_CHECKING:
     # pylint: disable=unused-import,ungrouped-imports
@@ -22,42 +23,51 @@ if TYPE_CHECKING:
 
     from azure.core.credentials_async import AsyncTokenCredential
 
-class DeviceUpdateClient:
-    """Device Update for IoT Hub is an Azure service that enables customers to publish update for their IoT devices to the cloud, and then deploy that update to their devices (approve updates to groups of devices managed and provisioned in IoT Hub). It leverages the proven security and reliability of the Windows Update platform, optimized for IoT devices. It works globally and knows when and how to update devices, enabling customers to focus on their business goals and let Device Update for IoT Hub handle the updates.
+class DeviceUpdateClient:  # pylint: disable=client-accepts-api-version-keyword
+    """Device Update for IoT Hub is an Azure service that enables customers to publish update for
+    their IoT devices to the cloud, and then deploy that update to their devices (approve updates
+    to groups of devices managed and provisioned in IoT Hub). It leverages the proven security and
+    reliability of the Windows Update platform, optimized for IoT devices. It works globally and
+    knows when and how to update devices, enabling customers to focus on their business goals and
+    let Device Update for IoT Hub handle the updates.
 
-    :ivar device_update: DeviceUpdateOperations operations
-    :vartype device_update: azure.iot.deviceupdate.aio.operations.DeviceUpdateOperations
-    :ivar device_management: DeviceManagementOperations operations
-    :vartype device_management: azure.iot.deviceupdate.aio.operations.DeviceManagementOperations
-    :param endpoint: Account endpoint.
-    :type endpoint: str
-    :param instance_id: Account instance identifier.
+    :ivar updates: UpdatesOperations operations
+    :vartype updates: azure.iot.deviceupdate.aio.operations.UpdatesOperations
+    :ivar devices: DevicesOperations operations
+    :vartype devices: azure.iot.deviceupdate.aio.operations.DevicesOperations
+    :ivar deployments: DeploymentsOperations operations
+    :vartype deployments: azure.iot.deviceupdate.aio.operations.DeploymentsOperations
+    :param account_endpoint: Account endpoint. Required.
+    :type account_endpoint: str
+    :param instance_id: Account instance identifier. Required.
     :type instance_id: str
-    :param credential: Credential needed for the client to connect to Azure.
+    :param credential: Credential needed for the client to connect to Azure. Required.
     :type credential: ~azure.core.credentials_async.AsyncTokenCredential
-    :keyword api_version: Api Version. The default value is "2021-06-01-preview". Note that
-     overriding this default value may result in unsupported behavior.
-    :paramtype api_version: str
-    :keyword int polling_interval: Default waiting time between two polls for LRO operations if no
-     Retry-After header is present.
     """
 
     def __init__(
         self,
-        endpoint: str,
+        account_endpoint: str,
         instance_id: str,
         credential: "AsyncTokenCredential",
         **kwargs: Any
     ) -> None:
-        _endpoint = 'https://{endpoint}'
-        self._config = DeviceUpdateClientConfiguration(endpoint=endpoint, instance_id=instance_id, credential=credential, **kwargs)
+        _endpoint = 'https://{accountEndpoint}'
+        self._config = DeviceUpdateClientConfiguration(account_endpoint=account_endpoint, instance_id=instance_id, credential=credential, **kwargs)
         self._client = AsyncPipelineClient(base_url=_endpoint, config=self._config, **kwargs)
 
         self._serialize = Serializer()
         self._deserialize = Deserializer()
         self._serialize.client_side_validation = False
-        self.device_update = DeviceUpdateOperations(self._client, self._config, self._serialize, self._deserialize)
-        self.device_management = DeviceManagementOperations(self._client, self._config, self._serialize, self._deserialize)
+        self.updates = UpdatesOperations(
+            self._client, self._config, self._serialize, self._deserialize
+        )
+        self.devices = DevicesOperations(
+            self._client, self._config, self._serialize, self._deserialize
+        )
+        self.deployments = DeploymentsOperations(
+            self._client, self._config, self._serialize, self._deserialize
+        )
 
 
     def send_request(
@@ -84,7 +94,7 @@ class DeviceUpdateClient:
 
         request_copy = deepcopy(request)
         path_format_arguments = {
-            "endpoint": self._serialize.url("self._config.endpoint", self._config.endpoint, 'str', skip_quote=True),
+            "accountEndpoint": self._serialize.url("self._config.account_endpoint", self._config.account_endpoint, 'str', skip_quote=True),
         }
 
         request_copy.url = self._client.format_url(request_copy.url, **path_format_arguments)
