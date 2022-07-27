f74986fa2d6494e6ff12f549c63f8260c7e3c0e2
SDKAuto <sdkautomation@microsoft.com>
2022-07-27 06:03:01 +0000

    CodeGen from PR 2563 in test-repo-billy/azure-rest-api-specs Merge 12870bb09e8181f67496b48d03ba34294f81f68a into bd5febeece66f1ec4a8f0f4661050db4185ed668

--- sdk/compute/azure-mgmt-imagebuilder/azure/mgmt/imagebuilder/_image_builder_client.py
+++ sdk/compute/azure-mgmt-imagebuilder/azure/mgmt/imagebuilder/_image_builder_client.py
@@ -9,20 +9,20 @@
 from copy import deepcopy
 from typing import Any, TYPE_CHECKING
 
-from msrest import Deserializer, Serializer
-
 from azure.core.rest import HttpRequest, HttpResponse
 from azure.mgmt.core import ARMPipelineClient
 
 from . import models
 from ._configuration import ImageBuilderClientConfiguration
+from ._serialization import Deserializer, Serializer
 from .operations import Operations, VirtualMachineImageTemplatesOperations
 
 if TYPE_CHECKING:
     # pylint: disable=unused-import,ungrouped-imports
     from azure.core.credentials import TokenCredential
 
-class ImageBuilderClient:
+
+class ImageBuilderClient:  # pylint: disable=client-accepts-api-version-keyword
     """Azure Virtual Machine Image Builder Client.
 
     :ivar virtual_machine_image_templates: VirtualMachineImageTemplatesOperations operations
@@ -30,10 +30,10 @@ class ImageBuilderClient:
      azure.mgmt.imagebuilder.operations.VirtualMachineImageTemplatesOperations
     :ivar operations: Operations operations
     :vartype operations: azure.mgmt.imagebuilder.operations.Operations
-    :param credential: Credential needed for the client to connect to Azure.
+    :param credential: Credential needed for the client to connect to Azure. Required.
     :type credential: ~azure.core.credentials.TokenCredential
     :param subscription_id: Subscription credentials which uniquely identify Microsoft Azure
-     subscription. The subscription Id forms part of the URI for every service call.
+     subscription. The subscription Id forms part of the URI for every service call. Required.
     :type subscription_id: str
     :param base_url: Service URL. Default value is "https://management.azure.com".
     :type base_url: str
@@ -58,15 +58,12 @@ class ImageBuilderClient:
         self._serialize = Serializer(client_models)
         self._deserialize = Deserializer(client_models)
         self._serialize.client_side_validation = False
-        self.virtual_machine_image_templates = VirtualMachineImageTemplatesOperations(self._client, self._config, self._serialize, self._deserialize)
+        self.virtual_machine_image_templates = VirtualMachineImageTemplatesOperations(
+            self._client, self._config, self._serialize, self._deserialize
+        )
         self.operations = Operations(self._client, self._config, self._serialize, self._deserialize)
 
-
-    def _send_request(
-        self,
-        request: HttpRequest,
-        **kwargs: Any
-    ) -> HttpResponse:
+    def _send_request(self, request: HttpRequest, **kwargs: Any) -> HttpResponse:
         """Runs the network request through the client's chained policies.
 
         >>> from azure.core.rest import HttpRequest
@@ -75,7 +72,7 @@ class ImageBuilderClient:
         >>> response = client._send_request(request)
         <HttpResponse: 200 OK>
 
-        For more information on this code flow, see https://aka.ms/azsdk/python/protocol/quickstart
+        For more information on this code flow, see https://aka.ms/azsdk/dpcodegen/python/send_request
 
         :param request: The network request you want to make. Required.
         :type request: ~azure.core.rest.HttpRequest
