5769d035425c6764f34ee4b7bfb5e29200cda48e
azure-sdk <no-reply@azure-sdk.com>
2022-07-29 04:51:27 +0000

    [auto]create by sdk generation

--- sdk/deviceupdate/azure-iot-deviceupdate/azure/iot/deviceupdate/operations/__init__.py
+++ sdk/deviceupdate/azure-iot-deviceupdate/azure/iot/deviceupdate/operations/__init__.py
@@ -6,16 +6,18 @@
 # Changes may cause incorrect behavior and will be lost if the code is regenerated.
 # --------------------------------------------------------------------------
 
-from ._operations import DeviceUpdateOperations
-from ._operations import DeviceManagementOperations
+from ._operations import UpdatesOperations
+from ._operations import DevicesOperations
+from ._operations import DeploymentsOperations
 
 from ._patch import __all__ as _patch_all
 from ._patch import *  # type: ignore # pylint: disable=unused-wildcard-import
 from ._patch import patch_sdk as _patch_sdk
 
 __all__ = [
-    "DeviceUpdateOperations",
-    "DeviceManagementOperations",
+    "UpdatesOperations",
+    "DevicesOperations",
+    "DeploymentsOperations",
 ]
 __all__.extend([p for p in _patch_all if p not in __all__])
 _patch_sdk()
