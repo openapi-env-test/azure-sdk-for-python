9987f54892678a85ca7fca6faa5287f5c4e4bcc8
SDKAuto <sdkautomation@microsoft.com>
2022-09-26 07:39:20 +0000

    CodeGen from PR 3581 in openapi-env-test/azure-rest-api-specs Update agfood.json (#3581)

--- sdk/agrifood/azure-mgmt-agrifood/azure/mgmt/agrifood/operations/__init__.py
+++ sdk/agrifood/azure-mgmt-agrifood/azure/mgmt/agrifood/operations/__init__.py
@@ -12,10 +12,16 @@ from ._farm_beats_models_operations import FarmBeatsModelsOperations
 from ._locations_operations import LocationsOperations
 from ._operations import Operations
 
+from ._patch import __all__ as _patch_all
+from ._patch import *  # type: ignore # pylint: disable=unused-wildcard-import
+from ._patch import patch_sdk as _patch_sdk
+
 __all__ = [
-    'ExtensionsOperations',
-    'FarmBeatsExtensionsOperations',
-    'FarmBeatsModelsOperations',
-    'LocationsOperations',
-    'Operations',
+    "ExtensionsOperations",
+    "FarmBeatsExtensionsOperations",
+    "FarmBeatsModelsOperations",
+    "LocationsOperations",
+    "Operations",
 ]
+__all__.extend([p for p in _patch_all if p not in __all__])
+_patch_sdk()
