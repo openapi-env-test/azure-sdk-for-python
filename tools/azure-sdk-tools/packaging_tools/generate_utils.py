6213f0b37f59edb7416e403506f35ea58cc00673
Yuchao Yan <yuchaoyan@microsoft.com>
2022-10-21 17:10:36 +0800

    Update generate_utils.py

--- tools/azure-sdk-tools/packaging_tools/generate_utils.py
+++ tools/azure-sdk-tools/packaging_tools/generate_utils.py
@@ -355,8 +355,8 @@ def gen_cadl(cadl_relative_path: str, spec_folder: str) -> Dict[str, Any]:
     output_path = Path(os.getcwd()) / project_yaml["emitters"][cadl_python]["sdk-folder"]
     if not output_path.exists():
         os.makedirs(output_path)
-        while(not output_path.exists()):
-            time.sleep(1)
+        time.sleep(10)
+            
 
     project_yaml["emitters"][cadl_python].pop("sdk-folder")
     project_yaml["emitters"][cadl_python]["output-path"] = str(output_path)
