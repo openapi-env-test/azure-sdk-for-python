6ddc48ddb0147b115bcc3bec62e1d3963cca30c9
SDKAuto <sdkautomation@microsoft.com>
2022-12-05 07:43:22 +0000

    CodeGen from PR 3612 in openapi-env-test/azure-rest-api-specs Merge 52679dca3bbc20f5233604817254b532f71494d4 into 675d58c2526751ae9bc8858d45a96c189df28833

--- sdk/agrifood/azure-mgmt-agrifood/azure/mgmt/agrifood/aio/__init__.py
+++ sdk/agrifood/azure-mgmt-agrifood/azure/mgmt/agrifood/aio/__init__.py
@@ -6,16 +6,18 @@
 # Changes may cause incorrect behavior and will be lost if the code is regenerated.
 # --------------------------------------------------------------------------
 
-from ._agri_food_mgmt_client import AgriFoodMgmtClient
+from ._azure_agri_food_rp_service import AzureAgriFoodRPService
 
 try:
     from ._patch import __all__ as _patch_all
-    from ._patch import *  # type: ignore # pylint: disable=unused-wildcard-import
+    from ._patch import *  # pylint: disable=unused-wildcard-import
 except ImportError:
     _patch_all = []
 from ._patch import patch_sdk as _patch_sdk
 
-__all__ = ["AgriFoodMgmtClient"]
+__all__ = [
+    "AzureAgriFoodRPService",
+]
 __all__.extend([p for p in _patch_all if p not in __all__])
 
 _patch_sdk()
--- sdk/agrifood/azure-mgmt-agrifood/azure/mgmt/agrifood/aio/operations/__init__.py
+++ sdk/agrifood/azure-mgmt-agrifood/azure/mgmt/agrifood/aio/operations/__init__.py
@@ -11,11 +11,9 @@ from ._farm_beats_extensions_operations import FarmBeatsExtensionsOperations
 from ._farm_beats_models_operations import FarmBeatsModelsOperations
 from ._locations_operations import LocationsOperations
 from ._operations import Operations
-from ._private_endpoint_connections_operations import PrivateEndpointConnectionsOperations
-from ._private_link_resources_operations import PrivateLinkResourcesOperations
 
 from ._patch import __all__ as _patch_all
-from ._patch import *  # type: ignore # pylint: disable=unused-wildcard-import
+from ._patch import *  # pylint: disable=unused-wildcard-import
 from ._patch import patch_sdk as _patch_sdk
 
 __all__ = [
@@ -24,8 +22,6 @@ __all__ = [
     "FarmBeatsModelsOperations",
     "LocationsOperations",
     "Operations",
-    "PrivateEndpointConnectionsOperations",
-    "PrivateLinkResourcesOperations",
 ]
 __all__.extend([p for p in _patch_all if p not in __all__])
 _patch_sdk()
