5ffaa012ddaaf2dabc300fd61e67b727a798980f
azure-sdk <no-reply@azure-sdk.com>
2023-01-03 02:08:33 +0000

    [auto]create by sdk generation

--- sdk/deviceupdate/azure-mgmt-deviceupdate/azure/mgmt/deviceupdate/_configuration.py
+++ sdk/deviceupdate/azure-mgmt-deviceupdate/azure/mgmt/deviceupdate/_configuration.py
@@ -6,6 +6,7 @@
 # Changes may cause incorrect behavior and will be lost if the code is regenerated.
 # --------------------------------------------------------------------------
 
+import sys
 from typing import Any, TYPE_CHECKING
 
 from azure.core.configuration import Configuration
@@ -14,13 +15,18 @@ from azure.mgmt.core.policies import ARMChallengeAuthenticationPolicy, ARMHttpLo
 
 from ._version import VERSION
 
+if sys.version_info >= (3, 8):
+    from typing import Literal  # pylint: disable=no-name-in-module, ungrouped-imports
+else:
+    from typing_extensions import Literal  # type: ignore  # pylint: disable=ungrouped-imports
+
 if TYPE_CHECKING:
     # pylint: disable=unused-import,ungrouped-imports
     from azure.core.credentials import TokenCredential
 
 
-class DeviceUpdateMgmtClientConfiguration(Configuration):  # pylint: disable=too-many-instance-attributes
-    """Configuration for DeviceUpdateMgmtClient.
+class DeviceUpdateConfiguration(Configuration):  # pylint: disable=too-many-instance-attributes
+    """Configuration for DeviceUpdate.
 
     Note that all parameters used to create this instance are saved as instance
     attributes.
@@ -35,8 +41,8 @@ class DeviceUpdateMgmtClientConfiguration(Configuration):  # pylint: disable=too
     """
 
     def __init__(self, credential: "TokenCredential", subscription_id: str, **kwargs: Any) -> None:
-        super(DeviceUpdateMgmtClientConfiguration, self).__init__(**kwargs)
-        api_version = kwargs.pop("api_version", "2022-10-01")  # type: str
+        super(DeviceUpdateConfiguration, self).__init__(**kwargs)
+        api_version: Literal["2022-10-01"] = kwargs.pop("api_version", "2022-10-01")
 
         if credential is None:
             raise ValueError("Parameter 'credential' must not be None.")
@@ -50,10 +56,7 @@ class DeviceUpdateMgmtClientConfiguration(Configuration):  # pylint: disable=too
         kwargs.setdefault("sdk_moniker", "mgmt-deviceupdate/{}".format(VERSION))
         self._configure(**kwargs)
 
-    def _configure(
-        self, **kwargs  # type: Any
-    ):
-        # type: (...) -> None
+    def _configure(self, **kwargs: Any) -> None:
         self.user_agent_policy = kwargs.get("user_agent_policy") or policies.UserAgentPolicy(**kwargs)
         self.headers_policy = kwargs.get("headers_policy") or policies.HeadersPolicy(**kwargs)
         self.proxy_policy = kwargs.get("proxy_policy") or policies.ProxyPolicy(**kwargs)
--- sdk/deviceupdate/azure-mgmt-deviceupdate/azure/mgmt/deviceupdate/aio/_configuration.py
+++ sdk/deviceupdate/azure-mgmt-deviceupdate/azure/mgmt/deviceupdate/aio/_configuration.py
@@ -6,6 +6,7 @@
 # Changes may cause incorrect behavior and will be lost if the code is regenerated.
 # --------------------------------------------------------------------------
 
+import sys
 from typing import Any, TYPE_CHECKING
 
 from azure.core.configuration import Configuration
@@ -14,13 +15,18 @@ from azure.mgmt.core.policies import ARMHttpLoggingPolicy, AsyncARMChallengeAuth
 
 from .._version import VERSION
 
+if sys.version_info >= (3, 8):
+    from typing import Literal  # pylint: disable=no-name-in-module, ungrouped-imports
+else:
+    from typing_extensions import Literal  # type: ignore  # pylint: disable=ungrouped-imports
+
 if TYPE_CHECKING:
     # pylint: disable=unused-import,ungrouped-imports
     from azure.core.credentials_async import AsyncTokenCredential
 
 
-class DeviceUpdateMgmtClientConfiguration(Configuration):  # pylint: disable=too-many-instance-attributes
-    """Configuration for DeviceUpdateMgmtClient.
+class DeviceUpdateConfiguration(Configuration):  # pylint: disable=too-many-instance-attributes
+    """Configuration for DeviceUpdate.
 
     Note that all parameters used to create this instance are saved as instance
     attributes.
@@ -35,8 +41,8 @@ class DeviceUpdateMgmtClientConfiguration(Configuration):  # pylint: disable=too
     """
 
     def __init__(self, credential: "AsyncTokenCredential", subscription_id: str, **kwargs: Any) -> None:
-        super(DeviceUpdateMgmtClientConfiguration, self).__init__(**kwargs)
-        api_version = kwargs.pop("api_version", "2022-10-01")  # type: str
+        super(DeviceUpdateConfiguration, self).__init__(**kwargs)
+        api_version: Literal["2022-10-01"] = kwargs.pop("api_version", "2022-10-01")
 
         if credential is None:
             raise ValueError("Parameter 'credential' must not be None.")
