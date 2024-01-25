67623a57b5a0129968c4baae3cd3620baa9c6b90
SDKAuto <sdkautomation@microsoft.com>
2024-01-25 22:49:16 +0000

    CodeGen from PR 3192 in test-repo-billy/azure-rest-api-specs Merge 51e469dffa6e69060b319e2a0f107a8e87bc6028 into 19077176ed509f8139ce021aca5b14b1924a1fd9

--- sdk/agrifood/azure-mgmt-agrifood/generated_samples/data_connectors_create_or_update.py
+++ sdk/agrifood/azure-mgmt-agrifood/generated_samples/data_connectors_create_or_update.py
@@ -14,7 +14,7 @@ from azure.mgmt.agrifood import AgriFoodMgmtClient
     pip install azure-identity
     pip install azure-mgmt-agrifood
 # USAGE
-    python farm_beats_models_get_operation_result.py
+    python data_connectors_create_or_update.py
 
     Before run the sample, please set the values of the client ID, tenant ID and client secret
     of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
@@ -26,18 +26,29 @@ from azure.mgmt.agrifood import AgriFoodMgmtClient
 def main():
     client = AgriFoodMgmtClient(
         credential=DefaultAzureCredential(),
-        solution_id="SOLUTION_ID",
         subscription_id="11111111-2222-3333-4444-555555555555",
     )
 
-    response = client.farm_beats_models.get_operation_result(
+    response = client.data_connectors.create_or_update(
         resource_group_name="examples-rg",
-        farm_beats_resource_name="examples-farmBeatsResourceName",
-        operation_results_id="resource-provisioning-id-farmBeatsResourceName",
+        data_manager_for_agriculture_resource_name="examples-dataManagerForAgricultureResourceName",
+        data_connector_name="WeatherIBM",
+        body={
+            "properties": {
+                "credentials": {
+                    "apiKey": {
+                        "keyName": "abcApiKey",
+                        "keyVaultUri": "https://testKeyVault.vault.azure.net/",
+                        "keyVersion": "239c0475c7d44f20b0fc27d3fe90a41d",
+                    },
+                    "kind": "ApiKeyAuthCredentials",
+                }
+            }
+        },
     )
     print(response)
 
 
-# x-ms-original-file: specification/agrifood/resource-manager/Microsoft.AgFoodPlatform/preview/2021-09-01-preview/examples/FarmBeatsModels_GetOperationResult.json
+# x-ms-original-file: specification/agrifood/resource-manager/Microsoft.AgFoodPlatform/preview/2023-06-01-preview/examples/DataConnectors_CreateOrUpdate.json
 if __name__ == "__main__":
     main()
